702cb49b1063243380f9b80cef12ced50357db3d
Cahya Wibawa <cahyawbwa.com>
2023-06-05 15:33:25 +0700

    Remove metadata and image from account layout

--- app/(account)/layout.tsx
+++ app/(account)/layout.tsx
@@ -1,14 +1,7 @@
 "use client";
-import { Metadata } from "next";
-import Image from "next/image";
 
 import { Separator } from "@/components/ui/Separator";
 import { SidebarNav } from "@/components/SideBarNav";
-
-export const metadata: Metadata = {
-	title: "Forms",
-	description: "Advanced form example using react-hook-form and Zod.",
-};
 
 const sidebarNavItems = [
 	{
@@ -19,19 +12,6 @@ const sidebarNavItems = [
 		title: "Profile",
 		href: "/profile",
 	},
-
-	// {
-	// 	title: "Appearance",
-	// 	href: "/examples/forms/appearance",
-	// },
-	// {
-	// 	title: "Notifications",
-	// 	href: "/examples/forms/notifications",
-	// },
-	// {
-	// 	title: "Display",
-	// 	href: "/examples/forms/display",
-	// },
 ];
 
 interface SettingsLayoutProps {
@@ -41,22 +21,6 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
 	return (
 		<>
-			{/* <div className="md:hidden">
-				<Image
-					src="/examples/forms-light.png"
-					width={1280}
-					height={791}
-					alt="Forms"
-					className="block dark:hidden"
-				/>
-				<Image
-					src="/examples/forms-dark.png"
-					width={1280}
-					height={791}
-					alt="Forms"
-					className="hidden dark:block"
-				/>
-			</div> */}
 			<div className="hidden space-y-6 p-10 pb-16 md:block">
 				<div className="space-y-0.5">
 					<h2 className="text-2xl font-bold tracking-tight">Settings</h2>
