a401a09738e6bae4151d1aa36b3caf0c4d6813df
Cahya Wibawa <cahyawbwa.com>
2023-06-20 22:49:26 +0700

    Add PanelRightClose icon to Navbar menu

--- app/components/layout/navbar/Navbar.tsx
+++ app/components/layout/navbar/Navbar.tsx
@@ -11,165 +11,178 @@ import Image from "next/image";
 import Link from "next/link";
 import { Fragment } from "react";
 import { CartButton } from "./CartButton";
-import { LogIn, User } from "lucide-react";
+import { LogIn, User, PanelRightClose } from "lucide-react";
 type Props = {
-	categories: swell.Category[];
+  categories: swell.Category[];
 };
 
 export default function Navbar({ categories }: Props) {
-	const opened = useMenu((state) => state.header);
-	const open = useMenu((state) => state.open);
-	const close = useMenu((state) => state.close);
+  const opened = useMenu((state) => state.header);
+  const open = useMenu((state) => state.open);
+  const close = useMenu((state) => state.close);
 
-	const handleOpen = () => open("header");
-	const handleClose = () => close("header");
+  const handleOpen = () => open("header");
+  const handleClose = () => close("header");
 
-	const { user, isLoading } = useUser();
+  const { user, isLoading } = useUser();
 
-	return (
-		<>
-			<Transition.Root show={opened} as={Fragment}>
-				<Dialog
-					as="div"
-					className="relative z-40 lg:hidden"
-					onClose={handleClose}
-				>
-					<Transition.Child
-						as={Fragment}
-						enter="transition-opacity ease-linear duration-300"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="transition-opacity ease-linear duration-300"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<div className="fixed inset-0 bg-black bg-opacity-25" />
-					</Transition.Child>
+  return (
+    <>
+      <Transition.Root show={opened} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-40 lg:hidden"
+          onClose={handleClose}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
 
-					<div className="fixed inset-0 z-40 flex">
-						<Transition.Child
-							as={Fragment}
-							enter="transition ease-in-out duration-300 transform"
-							enterFrom="-translate-x-full"
-							enterTo="translate-x-0"
-							leave="transition ease-in-out duration-300 transform"
-							leaveFrom="translate-x-0"
-							leaveTo="-translate-x-full"
-						>
-							<Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
-								<div className="flex px-4 pb-2 pt-5">
-									<button
-										type="button"
-										className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
-										onClick={handleClose}
-									>
-										<span className="sr-only">Close menu</span>
-										<XMarkIcon className="h-6 w-6" aria-hidden="true" />
-									</button>
-								</div>
+          <div className="fixed inset-0 z-40 flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-in-out duration-300 transform"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in-out duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-background pb-12 shadow-xl">
+                <div className="flex px-4 pb-2 pt-5">
+                  <button
+                    type="button"
+                    className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                    onClick={handleClose}
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
 
-								<div className="mt-2 space-y-6 border-gray-200 px-4 py-6">
-									{categories.map((category) => (
-										<div key={category.id} className="flow-root">
-											<Link
-												href={`/${category.slug}`}
-												onClick={handleClose}
-												className="-m-2 block p-2 font-medium text-gray-900"
-											>
-												{category.name}
-											</Link>
-										</div>
-									))}
-								</div>
+                <div className="mt-2 space-y-6 border-gray-200 px-4 py-6">
+                  {categories.map((category) => (
+                    <div key={category.id} className="flow-root">
+                      <Link
+                        href={`/${category.slug}`}
+                        onClick={handleClose}
+                        className="-m-2 block p-2 font-medium text-muted-foreground"
+                      >
+                        {category.name}
+                      </Link>
+                    </div>
+                  ))}
+                </div>
 
-								{!isLoading && !user && (
-									<div className="space-y-6 border-t border-gray-200 px-4 py-6">
-										<div className="flow-root">
-											<Link
-												href="/signin"
-												onClick={handleClose}
-												className="-m-2 block p-2 font-medium text-gray-900"
-											>
-												Sign in
-											</Link>
-										</div>
-										<div className="flow-root">
-											<Link
-												href="/signup"
-												onClick={handleClose}
-												className="-m-2 block p-2 font-medium text-gray-900"
-											>
-												Create account
-											</Link>
-										</div>
-									</div>
-								)}
-							</Dialog.Panel>
-						</Transition.Child>
-					</div>
-				</Dialog>
-			</Transition.Root>
+                {!isLoading && !user && (
+                  <div className="space-y-6 border-t border-gray-200 px-4 py-6">
+                    <div className="flow-root">
+                      <Link
+                        href="/signin"
+                        onClick={handleClose}
+                        className="-m-2 block p-2 font-medium text-gray-900"
+                      >
+                        Sign in
+                      </Link>
+                    </div>
+                    <div className="flow-root">
+                      <Link
+                        href="/signup"
+                        onClick={handleClose}
+                        className="-m-2 block p-2 font-medium text-gray-900"
+                      >
+                        Create account
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
 
-			<header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-20 bg-background/95 backdrop-blur">
-				<nav aria-label="Top">
-					<div className="border-b border-input">
-						<div className="flex h-16 items-center">
-							<button
-								type="button"
-								className="rounded-md bg-white p-2 text-gray-400 lg:hidden"
-								onClick={handleOpen}
-							>
-								<span className="sr-only">Open menu</span>
-								<Bars3Icon className="h-6 w-6" aria-hidden="true" />
-							</button>
+      <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-20 bg-background/95 backdrop-blur">
+        <nav aria-label="Top">
+          <div className="border-b border-input">
+            <div className="flex h-16 items-center">
+              <button
+                type="button"
+                className="rounded-md bg-background p-2 text-gray-400 lg:hidden"
+                onClick={handleOpen}
+              >
+                <span className="sr-only">Open menu</span>
+                <PanelRightClose className="h-6 w-6" aria-hidden="true" />
+              </button>
 
-							<div className="ml-4 flex lg:ml-0">
-								<Link href="/">
-									<span className="sr-only">
-										{process.env.NEXT_PUBLIC_SITE_NAME}
-									</span>
-									<Image
-										priority
-										className="mx-auto h-10 w-auto"
-										src="/white-logo.png"
-										alt=""
-										width={40}
-										height={40}
-									/>
-								</Link>
-							</div>
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <span className="sr-only">
+                    {process.env.NEXT_PUBLIC_SITE_NAME}
+                  </span>
+                  <Image
+                    priority
+                    className="mx-auto h-8 w-auto"
+                    src="/white-logo.png"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                </Link>
+              </div>
+              {/* Filter */}
+              <div className="hidden lg:ml-8 lg:block lg:self-stretch">
+                <div className="flex h-full space-x-8">
+                  <Link
+                    href="/filter"
+                    className="flex items-center self-center text-sm font-medium text-foreground transition-colors hover:text-foreground/80"
+                  >
+                    Shop
+                  </Link>
+                </div>
+              </div>
 
-							<div className="hidden lg:ml-8 lg:block lg:self-stretch">
-								<div className="flex h-full space-x-8">
-									{categories.map((category) => (
-										<Link
-											key={category.id}
-											href={`/${category.slug}`}
-											className="flex items-center self-center text-sm font-medium text-foreground transition-colors hover:text-foreground/80"
-										>
-											{category.name}
-										</Link>
-									))}
-								</div>
-							</div>
+              {/* Categories */}
 
-							<div className="ml-auto flex items-center space-x-2 ">
-								<div className="w-full flex-1 md:w-auto md:flex-none">
-									<CommandMenu />
-								</div>
-								<ModeToggle />
-								{!isLoading && (
-									<>
-										{user ? (
-											<div className="flex">
-												<AvatarDropdown />
-											</div>
-										) : (
-											<div className="mr-2 hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-												<Link href="/signin" className="text-sm font-medium">
-													<User aria-hidden="true" />
-												</Link>
-												{/* <span
+              <div className="hidden lg:ml-8 lg:block lg:self-stretch">
+                <div className="flex h-full space-x-8">
+                  {categories.map((category) => (
+                    <Link
+                      key={category.id}
+                      href={`/${category.slug}`}
+                      className="flex items-center self-center text-sm font-medium text-foreground transition-colors hover:text-foreground/80"
+                    >
+                      {category.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+
+              <div className="ml-auto flex items-center space-x-1 ">
+                <div className="w-full flex-1 md:w-auto md:flex-none">
+                  <CommandMenu />
+                </div>
+                <ModeToggle />
+                {!isLoading && (
+                  <>
+                    {user ? (
+                      <div className="flex">
+                        <AvatarDropdown />
+                      </div>
+                    ) : (
+                      <div className="mr-2 hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                        <Link href="/signin" className="text-sm font-medium">
+                          <User aria-hidden="true" />
+                        </Link>
+                        {/* <span
 													className="h-6 w-px bg-accent"
 													aria-hidden="true"
 												/>
@@ -183,12 +196,12 @@ export default function Navbar({ categories }: Props) {
 													className="h-6 w-px bg-accent"
 													aria-hidden="true"
 												/> */}
-											</div>
-										)}
-									</>
-								)}
-								<CartButton />
-								{/* <div className="ml-4 mr-2 flow-root lg:ml-6">
+                      </div>
+                    )}
+                  </>
+                )}
+                <CartButton />
+                {/* <div className="ml-4 mr-2 flow-root lg:ml-6">
                   <Link
                     href="/search"
                     className="group -m-2 flex items-center p-2"
@@ -202,12 +215,12 @@ export default function Navbar({ categories }: Props) {
                   </Link>
                 </div> */}
 
-								{/* <div className="ml-4 flow-root lg:ml-6"></div> */}
-							</div>
-						</div>
-					</div>
-				</nav>
-			</header>
-		</>
-	);
+                {/* <div className="ml-4 flow-root lg:ml-6"></div> */}
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
