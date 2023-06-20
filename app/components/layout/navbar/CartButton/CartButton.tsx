5b40da8edea0a41057c218fea8d95cf5a3e9d611
Cahya Wibawa <cahyawbwa.com>
2023-06-20 22:51:28 +0700

    Refactor CartButton component for readability and consistency

--- app/components/layout/navbar/CartButton/CartButton.tsx
+++ app/components/layout/navbar/CartButton/CartButton.tsx
@@ -8,194 +8,194 @@ import { Fragment } from "react";
 import useSWR, { useSWRConfig } from "swr";
 import { ShoppingBag } from "lucide-react";
 export default function CartButton() {
-	const opened = useMenu((state) => state.cart);
-	const open = useMenu((state) => state.open);
-	const close = useMenu((state) => state.close);
+  const opened = useMenu((state) => state.cart);
+  const open = useMenu((state) => state.open);
+  const close = useMenu((state) => state.close);
 
-	const handleOpen = () => open("cart");
-	const handleClose = () => close("cart");
+  const handleOpen = () => open("cart");
+  const handleClose = () => close("cart");
 
-	const { data } = useSWR("cart", getCart);
-	const { mutate } = useSWRConfig();
+  const { data } = useSWR("cart", getCart);
+  const { mutate } = useSWRConfig();
 
-	const counter = data?.item_quantity || 0;
+  const counter = data?.item_quantity || 0;
 
-	return (
-		<>
-			<button
-				type="button"
-				className="group -m-2 flex items-center p-2"
-				onClick={handleOpen}
-			>
-				<ShoppingBag
-					// className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-					aria-hidden="true"
-				/>
-				<span className="ml-2 text-sm font-medium text-foreground transition-colors hover:text-foreground/80">
-					{counter}
-				</span>
-				<span className="sr-only">items in cart, view bag</span>
-			</button>
-			<Transition.Root show={opened} as={Fragment}>
-				<Dialog as="div" className="relative z-30" onClose={handleClose}>
-					<Transition.Child
-						as={Fragment}
-						enter="ease-in-out duration-500"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="ease-in-out duration-500"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-					</Transition.Child>
+  return (
+    <>
+      <button
+        type="button"
+        className="group -m-2 flex items-center p-2"
+        onClick={handleOpen}
+      >
+        <ShoppingBag
+          // className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+          aria-hidden="true"
+        />
+        <span className="ml-2 text-sm font-medium text-foreground transition-colors hover:text-foreground/80">
+          {counter}
+        </span>
+        <span className="sr-only">items in cart, view bag</span>
+      </button>
+      <Transition.Root show={opened} as={Fragment}>
+        <Dialog as="div" className="relative z-30" onClose={handleClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
 
-					<div className="fixed inset-0 overflow-hidden">
-						<div className="absolute inset-0 overflow-hidden">
-							<div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-								<Transition.Child
-									as={Fragment}
-									enter="transform transition ease-in-out duration-500 sm:duration-700"
-									enterFrom="translate-x-full"
-									enterTo="translate-x-0"
-									leave="transform transition ease-in-out duration-500 sm:duration-700"
-									leaveFrom="translate-x-0"
-									leaveTo="translate-x-full"
-								>
-									<Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-										<div className="flex h-full flex-col overflow-y-scroll bg-background shadow-xl">
-											<div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-												<div className="flex items-start justify-between">
-													<Dialog.Title className="text-lg font-medium text-foreground">
-														Shopping cart
-													</Dialog.Title>
-													<div className="ml-3 flex h-7 items-center">
-														<button
-															type="button"
-															className="-m-2 p-2 text-gray-400 hover:text-gray-500"
-															onClick={handleClose}
-														>
-															<span className="sr-only">Close panel</span>
-															<XMarkIcon
-																className="h-6 w-6"
-																aria-hidden="true"
-															/>
-														</button>
-													</div>
-												</div>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-background shadow-xl">
+                      <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                        <div className="flex items-start justify-between">
+                          <Dialog.Title className="text-lg font-medium text-foreground">
+                            Shopping cart
+                          </Dialog.Title>
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                              onClick={handleClose}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </div>
+                        </div>
 
-												<div className="mt-8">
-													<div className="flow-root">
-														<ul
-															role="list"
-															className="-my-6 divide-y divide-gray-200"
-														>
-															{data?.items?.map((item) => (
-																<li key={item.id} className="flex py-6">
-																	<div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-																		<Image
-																			src={
-																				item.product?.images?.[0]?.file?.url ||
-																				""
-																			}
-																			alt={item.product?.name || ""}
-																			className="h-full w-full object-cover object-center"
-																			width={94}
-																			height={94}
-																		/>
-																	</div>
+                        <div className="mt-8">
+                          <div className="flow-root">
+                            <ul
+                              role="list"
+                              className="-my-6 divide-y divide-gray-200"
+                            >
+                              {data?.items?.map((item) => (
+                                <li key={item.id} className="flex py-6">
+                                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                    <Image
+                                      src={
+                                        item.product?.images?.[0]?.file?.url ||
+                                        ""
+                                      }
+                                      alt={item.product?.name || ""}
+                                      className="h-full w-full object-cover object-center"
+                                      width={94}
+                                      height={94}
+                                    />
+                                  </div>
 
-																	<div className="ml-4 flex flex-1 flex-col">
-																		<div>
-																			<div className="flex justify-between text-base font-medium text-foreground">
-																				<h3>
-																					<Link
-																						href={`/products/${item.product?.slug}`}
-																					>
-																						{item.product?.name}
-																					</Link>
-																				</h3>
-																				<p className="ml-4">
-																					{new Intl.NumberFormat("en-US", {
-																						style: "currency",
-																						currency: data.currency,
-																					}).format(item.price_total || 0)}
-																				</p>
-																			</div>
-																			<p className="mt-1 text-sm text-gray-500">
-																				{item.product?.id}
-																			</p>
-																		</div>
-																		<div className="flex flex-1 items-end justify-between text-sm">
-																			<p className="text-gray-500">
-																				Qty: {item.quantity}
-																			</p>
+                                  <div className="ml-4 flex flex-1 flex-col">
+                                    <div>
+                                      <div className="flex justify-between text-base font-medium text-foreground">
+                                        <h3>
+                                          <Link
+                                            href={`/products/${item.product?.slug}`}
+                                          >
+                                            {item.product?.name}
+                                          </Link>
+                                        </h3>
+                                        <p className="ml-4">
+                                          {new Intl.NumberFormat("en-US", {
+                                            style: "currency",
+                                            currency: data.currency,
+                                          }).format(item.price_total || 0)}
+                                        </p>
+                                      </div>
+                                      <p className="mt-1 text-sm text-gray-500">
+                                        {item.product?.id}
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-1 items-end justify-between text-sm">
+                                      <p className="text-gray-500">
+                                        Qty: {item.quantity}
+                                      </p>
 
-																			<div className="flex">
-																				<button
-																					type="button"
-																					className="font-medium text-indigo-600 hover:text-indigo-500"
-																					onClick={async () => {
-																						// rome-ignore lint/style/noNonNullAssertion: <explanation>
-																						await removeFromCart(item.id!);
-																						mutate("cart");
-																					}}
-																				>
-																					Remove
-																				</button>
-																			</div>
-																		</div>
-																	</div>
-																</li>
-															))}
-														</ul>
-													</div>
-												</div>
-											</div>
+                                      <div className="flex">
+                                        <button
+                                          type="button"
+                                          className="font-medium text-indigo-600 hover:text-indigo-500"
+                                          onClick={async () => {
+                                            // rome-ignore lint/style/noNonNullAssertion: <explanation>
+                                            await removeFromCart(item.id!);
+                                            mutate("cart");
+                                          }}
+                                        >
+                                          Remove
+                                        </button>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
 
-											<div className="border-t border-muted px-4 py-6 sm:px-6">
-												<div className="flex justify-between text-base font-medium text-foreground">
-													<p>Subtotal</p>
-													<p>
-														{new Intl.NumberFormat("en-US", {
-															style: "currency",
-															currency: data?.currency || "USD",
-														}).format(data?.sub_total || 0)}
-													</p>
-												</div>
-												<p className="mt-0.5 text-sm text-gray-500">
-													Shipping and taxes calculated at checkout.
-												</p>
-												<div className="mt-6">
-													<Link
-														href={data?.checkout_url || ""}
-														className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-													>
-														Checkout
-													</Link>
-												</div>
-												<div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-													<p>
-														or&nbsp;
-														<button
-															type="button"
-															className="font-medium text-indigo-600 hover:text-indigo-500"
-															onClick={handleClose}
-														>
-															Continue Shopping
-															<span aria-hidden="true"> &rarr;</span>
-														</button>
-													</p>
-												</div>
-											</div>
-										</div>
-									</Dialog.Panel>
-								</Transition.Child>
-							</div>
-						</div>
-					</div>
-				</Dialog>
-			</Transition.Root>
-		</>
-	);
+                      <div className="border-t border-muted px-4 py-6 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-foreground">
+                          <p>Subtotal</p>
+                          <p>
+                            {new Intl.NumberFormat("en-US", {
+                              style: "currency",
+                              currency: data?.currency || "USD",
+                            }).format(data?.sub_total || 0)}
+                          </p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">
+                          Shipping and taxes calculated at checkout.
+                        </p>
+                        <div className="mt-6">
+                          <Link
+                            href={data?.checkout_url || ""}
+                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          >
+                            Checkout
+                          </Link>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                          <p>
+                            or&nbsp;
+                            <button
+                              type="button"
+                              className="font-medium text-indigo-600 hover:text-indigo-500"
+                              onClick={handleClose}
+                            >
+                              Continue Shopping
+                              <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
 }
