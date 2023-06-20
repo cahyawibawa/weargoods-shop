29a2f9d832b62d6da88fc353cc203cca75ebdc42
Cahya Wibawa <cahyawbwa.com>
2023-06-20 22:42:38 +0700

    Refactor layout and add pagination to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,72 +12,72 @@ import { Toaster } from "@/components/ui/Toaster";
 import "./styles/globals.css";
 
 const inter = Inter({
-	variable: "--font-inter",
-	display: "swap",
-	subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+  subsets: ["latin"],
 });
 
 const roboto_mono = Roboto_Mono({
-	variable: "--font-roboto-mono",
-	display: "swap",
-	subsets: ["latin"],
+  variable: "--font-roboto-mono",
+  display: "swap",
+  subsets: ["latin"],
 });
 
 const { NEXT_PUBLIC_SITE_NAME } = process.env;
 
 export const metadata: Metadata = {
-	metadataBase: new URL(BASE_URL),
-	title: {
-		// rome-ignore lint/style/noNonNullAssertion: <explanation>
-		default: NEXT_PUBLIC_SITE_NAME!,
-		template: `%s | ${NEXT_PUBLIC_SITE_NAME}`,
-	},
-	robots: {
-		follow: true,
-		index: true,
-	},
-	description: "High-performance ecommerce store built with Next.js and Swell.",
-	openGraph: {
-		images: [
-			{
-				url: `/api/og?title=${encodeURIComponent(
-					process.env.NEXT_PUBLIC_SITE_NAME || "",
-				)}`,
-				width: 1200,
-				height: 630,
-			},
-		],
-		type: "website",
-	},
+  metadataBase: new URL(BASE_URL),
+  title: {
+    // rome-ignore lint/style/noNonNullAssertion: <explanation>
+    default: NEXT_PUBLIC_SITE_NAME!,
+    template: `%s | ${NEXT_PUBLIC_SITE_NAME}`,
+  },
+  robots: {
+    follow: true,
+    index: true,
+  },
+  description: "High-performance ecommerce store built with Next.js and Swell.",
+  openGraph: {
+    images: [
+      {
+        url: `/api/og?title=${encodeURIComponent(
+          process.env.NEXT_PUBLIC_SITE_NAME || ""
+        )}`,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export const revalidate = 1800;
 
 type Props = {
-	children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export default async function RootLayout({ children }: Props) {
-	const data = await getCategories();
-	return (
-		<html lang="en-US">
-			<head />
-			<body
-				className={cn(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable,
-				)}
-			>
-				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-					<div className="mx-auto flex h-full max-w-7xl flex-col px-4 sm:px-6 lg:px-8">
-						<NextTopLoader showSpinner={false} color="rgb(79 70 229)" />
-						<Navbar categories={data.results} />
-						{children}
-						<Footer />
-					</div>
-				</ThemeProvider>
-				<Toaster />
-			</body>
-		</html>
-	);
+  const data = await getCategories();
+  return (
+    <html lang="en-US">
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="mx-auto flex h-full max-w-7xl flex-col px-4 sm:px-6 lg:px-8">
+            <NextTopLoader showSpinner={false} color="rgb(79 70 229)" />
+            <Navbar categories={data.results} />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
+import Pagination from "./components/Pagination";
 import ProductCard from "./components/products/ProductCard";
 import { getProducts } from "./lib/swell/products";
-
 export const revalidate = 1800;
-
 export default async function Home() {
-	const data = await getProducts();
+  const data = await getProducts();
 
-	return (
-		<>
-			{/* <Banner /> */}
-			<div className="mt-4 grid grid-cols-1 border-l border-t border-input sm:grid-cols-2 md:mt-8 md:grid-cols-3 lg:mt-12 lg:grid-cols-4 xl:grid-cols-5">
-				{data.results.map((product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="mt-4 grid grid-cols-1 rounded-md border-l border-t border-input sm:grid-cols-2 md:mt-8 md:grid-cols-3 lg:mt-12 lg:grid-cols-4 xl:grid-cols-5">
+        {data.results.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <div className="mt-4">
+        <Pagination />
+      </div>
+    </>
+  );
 }
