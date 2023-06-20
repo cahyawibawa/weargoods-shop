50c96c00e0325c4953a47cea2d88a5b1a8ae515f
Cahya Wibawa <cahyawbwa.com>
2023-06-20 22:43:49 +0700

    Add search functionality to CommandMenu

--- app/components/CommandMenu.tsx
+++ app/components/CommandMenu.tsx
@@ -1,102 +1,105 @@
 "use client";
 
 import { DialogProps } from "@radix-ui/react-alert-dialog";
-import { useTheme } from "next-themes";
-import { useRouter } from "next/navigation";
-import useUser from "@/hooks/useUser";
 import * as React from "react";
-
 import { Button } from "@/components/ui/Button";
 import {
-	CommandDialog,
-	CommandEmpty,
-	CommandGroup,
-	CommandInput,
-	CommandItem,
-	CommandList,
-	CommandSeparator,
-	CommandShortcut,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
 } from "@/components/ui/Command";
-import {
-	Calculator,
-	Calendar,
-	CreditCard,
-	Settings,
-	Smile,
-	User,
-	ListFilter,
-	LogIn,
-} from "lucide-react";
-// import { docsConfig } from '@/config/docs'
+import { CreditCard, Settings, ListFilter, Shirt, Search } from "lucide-react";
+
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { getProducts } from "@/lib/swell/products";
+
 export function CommandMenu({ ...props }: DialogProps) {
-	const router = useRouter();
-	const [open, setOpen] = React.useState(false);
-	const { setTheme } = useTheme();
-	const { user, isLoading } = useUser();
+  const [open, setOpen] = React.useState(false);
+  const [search, setSearch] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState<swell.Product[]>([]);
 
-	React.useEffect(() => {
-		const down = (e: KeyboardEvent) => {
-			if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-				e.preventDefault();
-				setOpen((open) => !open);
-			}
-		};
+  const handleSearch = async () => {
+    const productsResponse = await getProducts({ search });
+    console.log(productsResponse);
+    const products = productsResponse.results;
+    setSearchResults(products);
+  };
 
-		document.addEventListener("keydown", down);
-		return () => document.removeEventListener("keydown", down);
-	}, []);
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
 
-	const runCommand = React.useCallback((command: () => unknown) => {
-		setOpen(false);
-		command();
-	}, []);
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
 
-	return (
-		<>
-			<Button
-				variant="outline"
-				className={cn(
-					"text-muted-foreground relative h-9 w-full justify-start rounded-[0.5rem] text-sm sm:pr-12 md:w-40 lg:w-64",
-				)}
-				onClick={() => setOpen(true)}
-				{...props}
-			>
-				<span className="hidden lg:inline-flex">Search products...</span>
-				<span className="inline-flex lg:hidden">Search...</span>
-				<kbd className="bg-muted pointer-events-none absolute right-1.5 top-2 hidden h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-					<span className="text-xs">⌘</span>K
-				</kbd>
-			</Button>
-			<CommandDialog open={open} onOpenChange={setOpen}>
-				<CommandInput placeholder="Type a command or search..." />
-				<CommandList>
-					<CommandEmpty>No results found.</CommandEmpty>
-					<CommandGroup heading="Suggestions">
-						<CommandItem>
-							<Calendar className="mr-2 h-4 w-4" />
-							<span>Calendar</span>
-						</CommandItem>
-						<CommandItem>
-							<Smile className="mr-2 h-4 w-4" />
-							<span>Search Emoji</span>
-						</CommandItem>
-						<CommandItem>
-							<Calculator className="mr-2 h-4 w-4" />
-							<span>Calculator</span>
-						</CommandItem>
-					</CommandGroup>
-					<CommandSeparator />
-					<CommandGroup heading="Settings">
-						<Link href="/filter">
-							<CommandItem>
-								<ListFilter className="mr-2 h-4 w-4" />
-								<span>Filter Products</span>
-								<CommandShortcut>⌘F</CommandShortcut>
-							</CommandItem>
-						</Link>
-						{/* <Link href="/signin">
+  const runCommand = React.useCallback((command: () => unknown) => {
+    setOpen(false);
+    command();
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "text-muted-foreground relative h-9 w-full justify-start rounded-[0.5rem] text-sm sm:pr-12 md:w-40 lg:w-64"
+        )}
+        onClick={() => setOpen(true)}
+        {...props}
+      >
+        <span className="hidden lg:inline-flex">Search products...</span>
+        <span className="inline-flex lg:hidden">
+          <Search className="h-6 w-6" />
+        </span>
+        <kbd className="bg-muted pointer-events-none absolute right-1.5 top-2 hidden h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput
+          value={search}
+          onValueChange={setSearch}
+          placeholder="Type a command or search..."
+          onSubmit={handleSearch}
+        />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandList>
+              {searchResults.map((product) => (
+                <CommandItem key={product.id}>
+                  <span>{product.name}</span>
+                </CommandItem>
+              ))}
+            </CommandList>
+
+            {/* <Link href="/clothes">
+              <CommandItem>
+                <Shirt className="mr-2 h-4 w-4" />
+                <span>Clothing</span>
+              </CommandItem>
+            </Link>
+            <CommandItem>
+              <ListFilter className="mr-2 h-4 w-4" />
+              <span>Filter Products</span>
+              <CommandShortcut>⌘F</CommandShortcut>
+            </CommandItem> */}
+          </CommandGroup>
+
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            {/* <Link href="/signin">
 							<CommandItem>
 								<LogIn className="mr-2 h-4 w-4" />
 								<span>Sign In</span>
@@ -108,21 +111,21 @@ export function CommandMenu({ ...props }: DialogProps) {
 							<span>Profile</span>
 							<CommandShortcut>⌘P</CommandShortcut>
 						</CommandItem> */}
-						<CommandItem>
-							<CreditCard className="mr-2 h-4 w-4" />
-							<span>Billing</span>
-							<CommandShortcut>⌘B</CommandShortcut>
-						</CommandItem>
-						<Link href="/user-account">
-							<CommandItem>
-								<Settings className="mr-2 h-4 w-4" />
-								<span>Settings</span>
-								<CommandShortcut>⌘S</CommandShortcut>
-							</CommandItem>
-						</Link>
-					</CommandGroup>
-				</CommandList>
-			</CommandDialog>
-		</>
-	);
+            <CommandItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <Link href="/user-account">
+              <CommandItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <CommandShortcut>⌘S</CommandShortcut>
+              </CommandItem>
+            </Link>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
 }
