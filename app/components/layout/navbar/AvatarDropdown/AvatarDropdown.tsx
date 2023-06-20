6ad0dbe6b00e56c50b540ce5d046e129e0232c37
Cahya Wibawa <cahyawbwa.com>
2023-06-20 22:49:38 +0700

    Refactor AvatarDropdown menu appearance

--- app/components/layout/navbar/AvatarDropdown/AvatarDropdown.tsx
+++ app/components/layout/navbar/AvatarDropdown/AvatarDropdown.tsx
@@ -10,69 +10,69 @@ import { useSWRConfig } from "swr";
 const userNavigation = [{ name: "Your Profile", href: "/profile" }];
 
 export default function AvatarDropdown() {
-	const router = useRouter();
-	const [isPending, startTransition] = useTransition();
-	const { mutate } = useSWRConfig();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const { mutate } = useSWRConfig();
 
-	return (
-		<Menu as="div" className="relative flex-shrink-0">
-			<Menu.Button className="p-2 text-gray-400 hover:text-gray-500">
-				<span className="sr-only">Open user menu</span>
-				<Avatar className="h-8 w-8">
-					<AvatarImage src="https://github.com/shadcn.png" />
-					<AvatarFallback>CN</AvatarFallback>
-				</Avatar>
-			</Menu.Button>
+  return (
+    <Menu as="div" className="relative flex-shrink-0">
+      <Menu.Button className="p-2 text-gray-400 hover:text-gray-500">
+        <span className="sr-only">Open user menu</span>
+        <Avatar className="h-8 w-8">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </Menu.Button>
 
-			<Transition
-				as={Fragment}
-				enter="transition ease-out duration-100"
-				enterFrom="transform opacity-0 scale-95"
-				enterTo="transform opacity-100 scale-100"
-				leave="transition ease-in duration-75"
-				leaveFrom="transform opacity-100 scale-100"
-				leaveTo="transform opacity-0 scale-95"
-			>
-				<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-background py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-					{userNavigation.map((item) => (
-						<Menu.Item key={item.name}>
-							{({ active }) => (
-								<Link
-									href={item.href}
-									className={classNames(
-										active ? "bg-gray-700/25" : "",
-										"block px-4 py-2 text-sm text-muted-foreground",
-									)}
-								>
-									{item.name}
-								</Link>
-							)}
-						</Menu.Item>
-					))}
-					<Menu.Item>
-						{({ active }) => (
-							<button
-								disabled={isPending}
-								type="button"
-								onClick={async () => {
-									await logoutUser();
-									startTransition(() => {
-										mutate("/api/me", null);
-										router.refresh();
-										router.push("/");
-									});
-								}}
-								className={classNames(
-									active ? "bg-gray-700/25" : "",
-									"block w-full px-4 py-2 text-left text-sm text-muted-foreground",
-								)}
-							>
-								Sign out
-							</button>
-						)}
-					</Menu.Item>
-				</Menu.Items>
-			</Transition>
-		</Menu>
-	);
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-background py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {userNavigation.map((item) => (
+            <Menu.Item key={item.name}>
+              {({ active }) => (
+                <Link
+                  href={item.href}
+                  className={classNames(
+                    active ? "bg-gray-300/25" : "",
+                    "block px-4 py-2 text-sm text-muted-foreground"
+                  )}
+                >
+                  {item.name}
+                </Link>
+              )}
+            </Menu.Item>
+          ))}
+          <Menu.Item>
+            {({ active }) => (
+              <button
+                disabled={isPending}
+                type="button"
+                onClick={async () => {
+                  await logoutUser();
+                  startTransition(() => {
+                    mutate("/api/me", null);
+                    router.refresh();
+                    router.push("/");
+                  });
+                }}
+                className={classNames(
+                  active ? "bg-gray-300/25" : "",
+                  "block w-full px-4 py-2 text-left text-sm text-muted-foreground"
+                )}
+              >
+                Sign out
+              </button>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
 }
