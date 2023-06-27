2ba765f0e9a91a85328bd8efa5140416241cc383
Cahya Wibawa <cahyawbwa.com>
2023-06-27 16:30:13 +0700

    add config for site name

---
+++ app/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "weargoods",
+  description:
+    "High-performance ecommerce store built with Next.js 13 and Swell.",
+  url: "https://github.com/cahyawibawa/weargoods-shop",
+  ogImage: "",
+
+  links: {
+    twitter: "https://twitter.com/lictoyagami",
+    github: "https://github.com/sadmann7/skateshop",
+  },
+};
