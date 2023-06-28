d526980171d16dbfad035e7f44a4cfd86959db97
Cahya Wibawa <cahyawbwa.com>
2023-06-28 22:58:13 +0700

    redesign auth form

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,90 +1,82 @@
 import { Metadata } from "next";
-import Image from "next/image";
 import Link from "next/link";
-import { Command } from "lucide-react";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
 import { cn } from "@/lib/utils";
+import { OAuthSignIn } from "@/components/auth/Oauth";
+import { SignInForm } from "@/components/form/SignInForm";
+import { Shell } from "@/components/Shell";
 import { buttonVariants } from "@/components/ui/Button";
-import { UserAuthForm } from "@/components/SignInAuthForm";
-
+import { Icons } from "@/components/Icons";
 export const metadata: Metadata = {
-	title: "Authentication",
-	description: "Authentication forms built using the components.",
+  title: "Sign In | Weargoods",
+  description: "Sign in to your Weargoods account.",
 };
 
 export default function AuthenticationPage() {
-	return (
-		<>
-			<div className="md:hidden">
-				<Image
-					src="/examples/authentication-light.png"
-					width={1280}
-					height={843}
-					alt="Authentication"
-					className="block dark:hidden"
-				/>
-				<Image
-					src="/examples/authentication-dark.png"
-					width={1280}
-					height={843}
-					alt="Authentication"
-					className="hidden dark:block"
-				/>
-			</div>
-			<div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-				<Link
-					href="/signup"
-					className={cn(
-						buttonVariants({ variant: "ghost", size: "sm" }),
-						"absolute right-4 top-4 md:right-8 md:top-8",
-					)}
-				>
-					Sign Up
-				</Link>
-				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-					<div
-						className="absolute inset-0 bg-cover"
-						style={{
-							backgroundImage:
-								"url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)",
-						}}
-					/>
-					<div className="relative z-20 flex items-center text-lg font-medium">
-						<Command className="mr-2 h-6 w-6" /> Weargoods
-					</div>
-					<div className="relative z-20 mt-auto">
-						<blockquote className="space-y-2">
-							<p className="text-lg">
-								&ldquo; If you want to find something that looks like a
-								different type of clothing you can find on Weargoods &rdquo;
-							</p>
-							<footer className="text-sm">Cahya</footer>
-						</blockquote>
-					</div>
-				</div>
-				<div className="lg:p-8">
-					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-						<div className="flex flex-col space-y-2 text-center">
-							<h1 className="text-2xl font-semibold tracking-tight">
-								Login to your account
-							</h1>
-							<p className="text-sm text-muted-foreground">
-								Enter your credentials below to continue.
-							</p>
-						</div>
-						<UserAuthForm />
-						<p className="px-8 text-center text-sm text-muted-foreground">
-							Don&apos;t have an account?{" "}
-							<Link
-								href="/signup"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Sign up
-							</Link>
-						</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
+      <Shell layout="auth">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Sign in</CardTitle>
+            <CardDescription>
+              Choose your preferred sign in method
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <OAuthSignIn />
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <SignInForm />
+          </CardContent>
+          <CardFooter className="flex flex-wrap items-center space-x-2">
+            <div className="flex-1 text-sm text-muted-foreground">
+              Don&apos;t have an account?{" "}
+              <Link
+                aria-label="Sign up"
+                href="/signup"
+                className="text-primary underline-offset-4 transition-colors hover:underline"
+              >
+                Sign up
+              </Link>
+            </div>
+            <Link
+              aria-label="Reset password"
+              href="/signin/reset-password"
+              className="text-sm text-primary underline-offset-4 transition-colors hover:underline"
+            >
+              Reset password
+            </Link>
+          </CardFooter>
+        </Card>
+      </Shell>
+    </div>
+  );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,98 +1,65 @@
 import { Metadata } from "next";
-import Image from "next/image";
 import Link from "next/link";
-import { Command } from "lucide-react";
-
-import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+import { OAuthSignIn } from "@/components/auth/Oauth";
+import { Shell } from "@/components/Shell";
+import { SignUpForm } from "@/components/form/SignUpForm";
 import { buttonVariants } from "@/components/ui/Button";
-import { SignUpAuthForm } from "@/components/SignUpAuthForm";
-
+import { Icons } from "@/components/Icons";
+import { cn } from "@/lib/utils";
 export const metadata: Metadata = {
-	title: "Authentication",
-	description: "Authentication forms built using the components.",
+  title: "Sign Up | Weargoods",
+  description: "Sign Up to your Weargoods account.",
 };
 
 export default function AuthenticationPage() {
-	return (
-		<>
-			<div className="md:hidden">
-				<Image
-					src="/examples/authentication-light.png"
-					width={1280}
-					height={843}
-					alt="Authentication"
-					className="block dark:hidden"
-				/>
-				<Image
-					src="/examples/authentication-dark.png"
-					width={1280}
-					height={843}
-					alt="Authentication"
-					className="hidden dark:block"
-				/>
-			</div>
-			<div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-				<Link
-					href="/login"
-					className={cn(
-						buttonVariants({ variant: "ghost", size: "sm" }),
-						"absolute right-4 top-4 md:right-8 md:top-8",
-					)}
-				>
-					Login
-				</Link>
-				<div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-					<div
-						className="absolute inset-0 bg-cover"
-						style={{
-							backgroundImage:
-								"url(https://images.unsplash.com/photo-1469386753370-153f56ba8e79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80)",
-						}}
-					/>
-					<div className="relative z-20 flex items-center text-lg font-medium">
-						<Command className="mr-2 h-6 w-6" /> Weargoods
-					</div>
-					<div className="relative z-20 mt-auto">
-						<blockquote className="space-y-2">
-							<p className="text-lg">
-								&ldquo; If you want to find something that looks like a
-								different type of clothing you can find on Weargoods &rdquo;
-							</p>
-							<footer className="text-sm">Cahya</footer>
-						</blockquote>
-					</div>
-				</div>
-				<div className="lg:p-8">
-					<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-						<div className="flex flex-col space-y-2 text-center">
-							<h1 className="text-2xl font-semibold tracking-tight">
-								Create an account
-							</h1>
-							<p className="text-sm text-muted-foreground">
-								Enter your details below to create your account.
-							</p>
-						</div>
-						<SignUpAuthForm />
-						<p className="px-8 text-center text-sm text-muted-foreground">
-							By clicking continue, you agree to our{" "}
-							<Link
-								href="/terms"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Terms of Service
-							</Link>{" "}
-							and{" "}
-							<Link
-								href="/privacy"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Privacy Policy
-							</Link>
-							.
-						</p>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <Link
+        href="/signin"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
+      <Shell layout="auth">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Sign Up</CardTitle>
+            <CardDescription>
+              Choose your preferred sign up method
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <OAuthSignIn />
+            <SignUpForm />
+          </CardContent>
+          <CardFooter className="flex flex-wrap items-center space-x-2">
+            <div className="flex-1 text-sm text-muted-foreground">
+              Already have an account?{" "}
+              <Link
+                aria-label="Sign in"
+                href="/signin"
+                className="text-primary underline-offset-4 transition-colors hover:underline"
+              >
+                Sign in
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      </Shell>
+    </div>
+  );
 }
