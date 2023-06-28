df565f712c4fafbba42ff7d73900c8ea8ee25b76
Cahya Wibawa <cahyawbwa.com>
2023-06-28 23:35:20 +0700

    Add BASE_URL constant and robots.txt function

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import { BASE_URL } from "@/constants";
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+    host: BASE_URL,
+  };
+}
