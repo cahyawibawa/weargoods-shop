ee0037cd1998f7600b0fcfd6751bafe40610a7c3
Cahya Wibawa <cahyawbwa.com>
2023-07-03 23:11:35 +0700

    Add storefront page and components

---
+++ app/(storefront)/[category]/[product]/page.tsx
@@ -0,0 +1,48 @@
+import ProductOverview from "@/components/products/ProductOverview";
+import { getProductBySlug } from "@/lib/swell/products";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+export const revalidate = 1800;
+
+type Props = {
+	params: {
+		product: string;
+	};
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	const { product } = params;
+
+	const productData = await getProductBySlug(product);
+
+	return {
+		title: productData?.name,
+		openGraph: {
+			images: [
+				{
+					url: `/api/og?title=${encodeURIComponent(productData?.name || "")}`,
+					width: 1200,
+					height: 630,
+				},
+			],
+			type: "website",
+		},
+	};
+}
+
+export default async function Product({ params }: Props) {
+	const { product } = params;
+
+	const data = await getProductBySlug(product);
+
+	if (!data) {
+		notFound();
+	}
+
+	return (
+		<div className="mt-4 grid md:mt-8 lg:mt-12">
+			<ProductOverview product={data} />
+		</div>
+	);
+}
---
+++ app/(storefront)/filter/page.tsx
@@ -0,0 +1,130 @@
+"use client";
+import { SidebarFilter } from "@/components/sidebar-filter";
+import ProductCard from "@/components/products/ProductCard";
+import { getProductFilters } from "@/lib/swell/filters";
+import { getProducts } from "@/lib/swell/products";
+import { redirect } from "next/navigation";
+import Pagination from "@/components/Pagination";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+type Props = {
+  searchParams: {
+    search?: string;
+    category?: string;
+    size?: string;
+    brands?: string;
+    color?: string;
+  };
+};
+
+type Filter = {
+  id: string;
+  label: string;
+  type: "range" | "select";
+  options: {
+    value: string | number;
+    label: string | number;
+  }[];
+  interval?: number;
+};
+
+// rome-ignore lint/suspicious/noRedeclare: <explanation>
+export default function Filter({ searchParams }: Props) {
+  const search = searchParams.search;
+  const category = searchParams.category?.split(",");
+  const brands = searchParams.brands?.split(",");
+  const size = searchParams.size?.split(",");
+  const color = searchParams.color?.split(",");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [productData, setProductData] = useState<
+    (swell.Product & { categories: swell.Category[] })[]
+  >([]);
+  const [filters, setFilters] = useState<Filter[]>([]); // Use the custom Filter type
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async (page: number) => {
+    setIsLoading(true);
+    const { results, total_pages } = await getProducts({ page });
+    setTotalPages(total_pages);
+    setProductData(results);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const fetchDataAndRedirect = async () => {
+    const products = await getProducts({
+      search,
+      $filters: {
+        category,
+        size,
+        brands,
+        color,
+      },
+    });
+    if (products.results.length === 0) {
+      redirect("/filter");
+    }
+    const filters = await getProductFilters(products.results);
+    setFilters(filters);
+    setProductData(products.results);
+    setTotalPages(products.total_pages);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchDataAndRedirect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search, category, brands, size, color]);
+
+  return (
+    <main className="mt-4 md:mt-8 lg:mt-12">
+      <div className="border-b border-input pb-10">
+        <Image
+          src="/images/shop-men.jpg"
+          alt="Banner"
+          className="w-full object-cover"
+          width={1920}
+          height={500}
+        />
+      </div>
+
+      <div className="pt-12 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
+        <SidebarFilter filters={filters} />
+
+        <div className="lg:col-span-2 xl:col-span-3">
+          <div className="grid grid-cols-1 border-l border-t border-input md:grid-cols-2 lg:col-span-2 lg:grid-cols-3 xl:col-span-3 xl:grid-cols-4">
+            {isLoading
+              ? [...Array(20)].map((_, index) => (
+                  // rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                  <ProductCard key={index} isLoading={true} />
+                ))
+              : productData.map((product) => (
+                  <ProductCard
+                    key={product.id}
+                    product={product}
+                    isLoading={false}
+                  />
+                ))}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onChangePage={handlePageChange}
+        />
+      </div>
+    </main>
+  );
+}
