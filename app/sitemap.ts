31c5a969b593cb3c89adbdf0a0c8f8ff6cdb9eed
Cahya Wibawa <cahyawbwa.com>
2023-07-08 16:00:52 +0700

    Refactor sitemap.ts for readability and maintainability

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,34 +6,34 @@ import { MetadataRoute } from "next";
 export const revalidate = 1800;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-	const routesMap = [
-		"",
-		"/login",
-		"/signup",
-		"/forgot-password",
-		"/search",
-	].map((route) => ({
-		url: `${BASE_URL}${route}`,
-		lastModified: new Date().toISOString(),
-	}));
+  const routesMap = [
+    "",
+    "/login",
+    "/signup",
+    "/forgot-password",
+    "/search",
+  ].map((route) => ({
+    url: `${BASE_URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
 
-	const categoriesPromise = getCategories().then((categories) =>
-		categories.results.map((category) => ({
-			url: `${BASE_URL}/${category.slug}`,
-			lastModified: new Date().toISOString(),
-		})),
-	);
+  const categoriesPromise = getCategories().then((categories) =>
+    categories.results.map((category) => ({
+      url: `${BASE_URL}/${category.slug}`,
+      lastModified: new Date().toISOString(),
+    }))
+  );
 
-	const productsPromise = getProducts().then((products) =>
-		products.results.map((product) => ({
-			url: `${BASE_URL}/${product.categories[0].slug}/${product.slug}`,
-			lastModified: new Date().toISOString(),
-		})),
-	);
+  const productsPromise = getProducts().then((products) =>
+    products.results.map((product) => ({
+      url: `${BASE_URL}/${product.categories[0].slug}/${product.slug}`,
+      lastModified: new Date().toISOString(),
+    }))
+  );
 
-	const fetchedRoutes = (
-		await Promise.all([categoriesPromise, productsPromise])
-	).flat();
+  const fetchedRoutes = (
+    await Promise.all([categoriesPromise, productsPromise])
+  ).flat();
 
-	return [...routesMap, ...fetchedRoutes];
+  return [...routesMap, ...fetchedRoutes];
 }
