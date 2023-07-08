ec4b1deb5b67e03eef675fb23137ad281cc167cf
Cahya Wibawa <cahyawbwa.com>
2023-07-08 15:53:50 +0700

    refactor sidebarNav

--- app/components/SideBarNav.tsx
+++ app/components/SideBarNav.tsx
@@ -2,53 +2,56 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import type { SidebarNavItem } from "types";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/Button";
-import { Icons } from "./Icons";
+import { Icons } from "@/components/Icons";
 
-interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
-  items: {
-    href: string;
-    title: string;
-  }[];
+export interface SidebarNavProps {
+  items: SidebarNavItem[];
 }
 
-export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
+export function SidebarNav({ items }: SidebarNavProps) {
   const pathname = usePathname();
 
+  if (!items?.length) return null;
+
   return (
-    <nav
-      className={cn(
-        "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
-        className
-      )}
-      {...props}
-    >
-      {items.map((item) => (
-        <Link
-          key={item.href}
-          href={item.href}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            pathname === item.href
-              ? "bg-muted hover:bg-muted"
-              : "hover:bg-muted hover:text-foreground",
-            "justify-start"
-          )}
-        >
-          {item.title === "Account" && (
-            <Icons.user className="mr-2 h-4 w-4" aria-hidden="true" /> // Use the appropriate icon component from the Icons component
-          )}
-          {item.title === "Dashboard" && (
-            <Icons.terminal className="mr-2 h-4 w-4" aria-hidden="true" /> // Use the appropriate icon component from the Icons component
-          )}
-          {item.title === "Orders" && (
-            <Icons.scrollText className="mr-2 h-4 w-4" aria-hidden="true" /> // Use the appropriate icon component from the Icons component
-          )}
-          {item.title}
-        </Link>
-      ))}
-    </nav>
+    <div className="flex w-full flex-col gap-2">
+      {items.map((item, index) => {
+        const Icon = Icons[item.icon ?? "chevronLeft"];
+
+        return item.href ? (
+          <Link
+            // rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={index}
+            href={item.href}
+            target={item.external ? "_blank" : ""}
+            rel={item.external ? "noreferrer" : ""}
+          >
+            <span
+              className={cn(
+                "group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:bg-muted hover:text-foreground",
+                pathname === item.href
+                  ? "bg-muted font-medium text-foreground"
+                  : "text-muted-foreground",
+                item.disabled && "pointer-events-none opacity-60"
+              )}
+            >
+              <Icon className="mr-2 h-4 w-4" aria-hidden="true" />
+              <span>{item.title}</span>
+            </span>
+          </Link>
+        ) : (
+          <span
+            // rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={index}
+            className="flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline"
+          >
+            {item.title}
+          </span>
+        );
+      })}
+    </div>
   );
 }
---
+++ app/config/dashboard.ts
@@ -0,0 +1,28 @@
+import type { SidebarNavItem } from "types";
+
+export type DashboardConfig = {
+  sidebarNav: SidebarNavItem[];
+}; // <-- This is the type of the dashboardConfig object
+
+export const dashboardConfig: DashboardConfig = {
+  sidebarNav: [
+    {
+      title: "Account",
+      href: "/dashboard/account",
+      icon: "user",
+      items: [],
+    },
+    {
+      title: "Stores",
+      href: "/dashboard/store",
+      icon: "store",
+      items: [],
+    },
+    {
+      title: "Orders",
+      href: "/dashboard/orders",
+      icon: "scrollText",
+      items: [],
+    },
+  ],
+};
